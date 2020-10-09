20cfe7562220ab87b6cc230ee51b260393751eb2
thutotran <thuto.tranthi@gmail.com>
2020-10-09 18:45:46 +0700

    [ADD] Variables, data types, and operators exercise

---
+++ module1/Bien_kieu_du_lieu_va_toam_tu/1.Thuc_hanh/area.js
@@ -0,0 +1,12 @@
+// Khai bao bien
+let inputWidth;
+let inputHeight;
+//Lay du lieu nhap
+inputWidth = prompt('Enter the width');
+inputHeight = prompt('Enter the height');
+// Chuyen kieu du lieu
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+// Tinh dien tich
+let area = width * height;
+document.write('The area is: ' + area);
--- module1/Bien_kieu_du_lieu_va_toam_tu/1.Thuc_hanh/operators-script.js
+++ module1/Bien_kieu_du_lieu_va_toam_tu/1.Thuc_hanh/operators-script.js
@@ -1,1 +1,4 @@
-let inputWidht
+let width = 20;
+let height = 10;
+let area = width * height;
+document.write('Area = ' + area);
